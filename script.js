077f0d7b402d630de35736996ccd9490af05b82f
Pyrite9 <arlaehdrjs@gmail.com>
2026-05-21 03:15:22 +0900

    todo list 완성 / 파일 분리 완료

---
+++ script.js
@@ -0,0 +1,93 @@
+const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
+
+let todos = [];
+
+function todoInit() {
+    todos = JSON.parse(localStorage.getItem("todo")) || [];
+    todoDisplay();
+}
+
+function handleKeyDown(event) {
+    if (event.key === "Enter") {
+        addTodo();
+    }
+}
+
+function addTodo() {
+    let todo = todoInput.value.trim();
+    
+    if (todo === "") return;
+    
+    todos.push({text: todo, done: false});
+    
+    todoInput.value = "";
+
+    localStorage.setItem("todo", JSON.stringify(todos));
+
+    todoDisplay();
+}
+
+function todoDisplay() {
+
+    if (todos.length === 0) {
+        todoList.innerHTML = `<p>할 일이 없습니다. 할 일을 추가해보세요.</p>`;
+        return;
+    }
+
+    let html = "";
+
+    for (let i = 0; i < todos.length; i++) {
+        html += `
+            <div id="todo-item-${i}" class="${todos[i].done ? "done" : ""}"> 
+                <input type="checkbox" ${todos[i].done ? "checked" : ""} onclick="toggleDone(${i})">
+                <span id="todo-text-${i}" onclick="editTodo(${i})">${todos[i].text}</span>
+                <button class="deleteBtn" onclick="deleteTodo(${i})">X</button>
+            </div>
+        `;
+    }
+
+    todoList.innerHTML = html;
+
+}
+
+function toggleDone(n) {
+    todos[n].done = !todos[n].done;
+
+    localStorage.setItem("todo", JSON.stringify(todos));
+
+    todoDisplay();
+}
+
+function editTodo(n) {
+    const span = document.querySelector(`#todo-text-${n}`);
+
+    span.outerHTML = `
+        <input id="todo-text-${n}" type="text" value="${todos[n].text}" onblur="saveTodo(${n}, this.value)" onkeydown="if(event.key==='Enter') this.blur()">
+    `;
+}
+
+function saveTodo(n, val) {
+
+    if (val.trim() === "") {
+        todoDisplay();
+        return;
+    }
+
+    todos[n].text = val;
+
+    localStorage.setItem("todo", JSON.stringify(todos));
+
+    todoDisplay();
+}
+
+function deleteTodo(n) {
+    todos.splice(n, 1);
+
+    localStorage.setItem("todo", JSON.stringify(todos));
+
+    todoDisplay();
+}
+
+todoInit();
+
